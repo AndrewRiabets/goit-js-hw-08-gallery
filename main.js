407f4c878acf0c629f059bfc0421584ded1388d2
AndrewRiabets <Inferion5@gmail.com>
2021-07-25 16:39:59 +0300

    готовое дз

---
+++ main.js
@@ -0,0 +1,26 @@
+// const refs = {
+//     galleryRefs: document.querySelector('js-gallery')
+// };
+
+// console.log(createGalleryMarkup(galleryItems))
+
+// // Создаю разметку лишек
+// function createGalleryMarkup(galleryItems) {
+//     const markup = galleryItems.map(item => {
+//         return `
+//         <li class="gallery__item">
+//             <a
+//             class="gallery__link"
+//             href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//             >
+//         <img
+//             class="gallery__image"
+//             src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+//             data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//             alt="Tulips"
+//         />
+//             </a>
+//         </li>`
+//     })
+//     console.log(markup)
+//  };
